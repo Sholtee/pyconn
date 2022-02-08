b25f9523f32d6841b545261650da075c1cc7c510
Dénes Solti <sodnaatx@gmail.com>
2022-02-08 19:43:54 +0100

    add some create_api() related tests

--- tests/unit/core_tests.py
+++ tests/unit/core_tests.py
@@ -3,7 +3,7 @@
 # Author: Denes Solti
 
 from importlib import reload
-from turtle import st
+from types import MethodType
 from unittest import TestCase, TextTestRunner, defaultTestLoader
 from unittest.mock import MagicMock, patch
 
@@ -92,6 +92,57 @@ class ApiConnectionTests(TestCase):
         conn = core.ApiConnection('http://localhost:1986/api')
         conn.sessionid = 'cica'
         self.assertEqual(conn.invoke('IModule', 'Method', []), 3)
+
+    @patch('urllib.request.urlopen')
+    def test_create_api_should_throw_if_the_schema_cannot_be_found(self, mock_urlopen):
+        mock_response = MagicMock()
+        mock_response.status = 200
+        mock_response.headers = {'content-type': 'application/json'}
+        mock_response.__enter__.return_value = mock_response
+        mock_response.read.return_value = '{}'
+        mock_urlopen.return_value = mock_response
+
+        reload(core)
+
+        conn = core.ApiConnection('http://localhost:1986/api')
+        with self.assertRaises(Exception) as error:
+            conn.create_api('IModule')
+        self.assertEqual(str(error.exception), 'Schema could not be found')
+
+    @patch('urllib.request.urlopen')
+    def test_create_api_should_process_method_descriptors(self, mock_urlopen):
+        mock_response = MagicMock()
+        mock_response.status = 200
+        mock_response.headers = {'content-type': 'application/json'}
+        mock_response.__enter__.return_value = mock_response
+        mock_response.read.return_value = '{"IModule": {"Methods": {"MethodA": {}}, "Properties": {}}}'
+        mock_urlopen.return_value = mock_response
+
+        reload(core)
+
+        conn = core.ApiConnection('http://localhost:1986/api')
+        api = conn.create_api('IModule')
+        self.assertTrue(hasattr(api, 'method_a'))
+        self.assertIsInstance(api.method_a, MethodType)
+
+    @patch('urllib.request.urlopen')
+    def test_create_api_should_process_property_descriptors(self, mock_urlopen):
+        mock_response = MagicMock()
+        mock_response.status = 200
+        mock_response.headers = {'content-type': 'application/json'}
+        mock_response.__enter__.return_value = mock_response
+        mock_response.read.return_value = '{"IModule": {"Methods": {}, "Properties": {"Prop": {"HasGetter": true, "HasSetter": false}}}}'
+        mock_urlopen.return_value = mock_response
+
+        reload(core)
+
+        conn = core.ApiConnection('http://localhost:1986/api')
+        api = conn.create_api('IModule')
+
+        prop = getattr(api.__class__, 'prop', None)
+
+        self.assertTrue(prop)
+        self.assertIsInstance(prop, property)
 
 if __name__ == '__main__':
     runner = TextTestRunner()
