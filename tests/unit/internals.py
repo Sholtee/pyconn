35a67e124f549ac58bc24c62ef19c56d62bbb2f5
Dénes Solti <sodnaatx@gmail.com>
2022-02-08 14:00:52 +0100

    use subtests

--- tests/unit/internals.py
+++ tests/unit/internals.py
@@ -8,14 +8,9 @@ from pyconn.internals import _load_json, _snake_case
 
 class InternalsTests(TestCase):
     def test_snake_case(self):
-        self.assertEqual(_snake_case('cica'), 'cica')
-        self.assertEqual(_snake_case('Cica'), 'cica')
-        self.assertEqual(_snake_case('CicaMica'), 'cica_mica')
-        self.assertEqual(_snake_case('cicaMica'), 'cica_mica')
-        self.assertEqual(_snake_case('ABC'), 'ABC')
-        self.assertEqual(_snake_case('PropB'), 'prop_b')
-        self.assertEqual(_snake_case('dABC'), 'd_ABC')
-        self.assertEqual(_snake_case('ABCd'), 'ABC_d')
+        for (str, expected) in [('cica', 'cica'), ('Cica', 'cica'), ('CicaMica', 'cica_mica'), ('cicaMica', 'cica_mica'), ('ABC', 'ABC'), ('PropB', 'prop_b'), ('dABC', 'd_ABC'), ('ABCd', 'ABC_d')]:
+            with self.subTest():
+                self.assertEqual(_snake_case(str), expected)
 
     def test_load_json(self):
         val = _load_json('{"Prop": 1, "PropB": {"ValA": 1986}, "propC": [{"propD": "cica"}]}')
